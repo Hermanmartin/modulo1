7fcba5eb6c7b11bbcbc0c991163cb4e0def8fd51
Martin Herman <mherman.alumnohenry@gmail.com>
2023-11-15 09:27:39 -0300

    modulo1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,11 +1,22 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = parseInt(num,2);
+
+   return decimal;
+}
+let numeroBinario = 1011;
+BinarioADecimal(numeroBinario);
 
 
+function DecimalABinario(num) {
+   let binario = num.toString(2);
 
-function DecimalABinario(num) {}
-  
+   return binario;
+}
+
+let numeroDecimal = 11;
+DecimalABinario(numeroDecimal);
 
 module.exports = {
    BinarioADecimal,
